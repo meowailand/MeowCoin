ecec654e049185debfb3e13606dab2eeecd2850a
meowpunch <jinhoonbae@gmail.com>
2021-06-30 22:43:10 +0200

    JB: simple blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
 	data     string
 	hash     string
@@ -7,15 +13,52 @@ type block struct {
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
+}
+
+func (b block) PrintBlock() {
+	fmt.Printf("data: %s\n", b.data)
+	fmt.Printf("hash: %s\n", b.hash)
+	fmt.Printf("prevHash: %s\n", b.prevHash)
 }
 
 // the singleton pattern is a software design pattern that restricts the instantiation of a class to one "single" instance
 var b *blockchain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.data + b.prevHash))
+	b.hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHas() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHas()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockChain() *blockchain {
 	if b == nil {
-		b = &blockchain{}
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
 	}
 	return b
+}
+
+func (b * blockchain) AllBlocks() []*block {
+	return b.blocks
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/meowailand/MeowCoin/blockchain"
+import (
+	"github.com/meowailand/MeowCoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		block.PrintBlock()
+	}
 }
